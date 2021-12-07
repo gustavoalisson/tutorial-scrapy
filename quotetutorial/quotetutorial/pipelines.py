0b9daed24e429fb82415f88d2ad121b947de0d17
gustavoalisson <gustavoalisson112@gmail.com>
2021-12-07 01:07:48 -0300

    configuração pipeline

--- quotetutorial/quotetutorial/pipelines.py
+++ quotetutorial/quotetutorial/pipelines.py
@@ -5,9 +5,25 @@
 
 
 # useful for handling different item types with a single interface
+
+
+# Sempre que um item é recebido, ele decide uma das seguintes ações -
+
+# Continue processando o item.
+# Solte-o do pipeline.
+# Pare de processar o item.
+# Os pipelines de itens são geralmente usados ​​para os seguintes fins -
+
+# Armazenando itens raspados no banco de dados.
+# Se o item recebido for repetido, ele descartará o item repetido.
+# Ele irá verificar se o item está com campos direcionados.
+# Limpando dados HTML.
+
+
 from itemadapter import ItemAdapter
 
 
 class QuotetutorialPipeline:
     def process_item(self, item, spider):
+        print('Pipeline: ' + item['title'][0])
         return item
